69d52e766b9ffb2c80d233842021e9b1c23176d5
Hubert <hubert.zajac92@gmail.com>
2022-09-05 14:35:33 +0200

    Add basic structure and set connection with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PlacesModule } from './places/places.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'places',
+      entities: [],
+      synchronize: true,
+    }),
+    PlacesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
